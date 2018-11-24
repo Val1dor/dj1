6a54fd0b95aa610ee01b73105fa022f20b7aadf4
Val1dor <45298449+Val1dor@users.noreply.github.com>
2018-11-24 17:45:53 +0100

    manytomany

--- tutorial/admin.py
+++ tutorial/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Article, Order, Supplier, Detail
 
 # Register your models here.
--- tutorial/models.py
+++ tutorial/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import CharField
+from django.contrib import admin
 
 class Sensor(models.Model):
     # class Meta:
@@ -9,9 +10,10 @@ class Sensor(models.Model):
     SensorStatus = models.IntegerField()
 
 
+
 class Supplier(models.Model):
     supplier_id = models.AutoField(primary_key=True)
-    supplier_name = models.CharField( max_length=200)
+    supplier_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.supplier_name
@@ -21,8 +23,7 @@ class Article(models.Model):
     article_label = models.CharField(max_length=200)
     article_sensor_no = models.IntegerField()
     article_sensor_status = models.BooleanField(default="True")
-    article_supplier = models.ManyToManyField('Supplier', through='Detail')
-
+    article_supplier = models.ManyToManyField(Supplier)#, through='Detail')
 
     def __str__(self):
         return self.article_label
@@ -39,3 +40,7 @@ class Order(models.Model):
     order_article = models.ForeignKey(Article, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=200)
     order_date = models.DateField()
+
+
+
+
